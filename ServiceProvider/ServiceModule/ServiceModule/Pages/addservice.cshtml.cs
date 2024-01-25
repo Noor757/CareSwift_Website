2b82695704413348008716980d1509b5050578cf
Noor757 <122727687+Noor757@users.noreply.github.com>
2024-01-26 00:36:57 +0500

    CSW

---
+++ ServiceProvider/ServiceModule/ServiceModule/Pages/addservice.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ServiceModule.Pages
+{
+    public class addserviceModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ ServiceProvider/ServiceModule/ServiceModule/Pages/forgetpassword.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ServiceModule.Pages
+{
+    public class forgetpasswordModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
